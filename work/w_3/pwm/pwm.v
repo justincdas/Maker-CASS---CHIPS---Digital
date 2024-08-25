6b919b537739e8d34f42fe032374be35686be7fe
justin <justin@Ubuntu.myguest.virtualbox.org>
2024-08-25 10:47:57 +0530

    update

--- work/w_3/pwm/pwm.v
+++ work/w_3/pwm/pwm.v
@@ -1,19 +1,26 @@
 module pwm (
             input            clk,
+            input            rst_n,
             output reg [3:0] pwm_out
            );
 
   reg [6:0] cntr = 0;
 
-  always@ (posedge clk) begin
-    if (cntr < 100) begin
+  always@ (posedge clk, negedge rst_n) begin
+    if(rst_n == 1'b0) begin
+      cntr    <= 7'b0;
+      pwm_out <= 4'b0;
+    end else begin
       cntr <= cntr+1;
-      pwm_out[0] <= (cntr < 25 ) ? 1'b1 : 1'b0;
-      pwm_out[1] <= (cntr < 50 ) ? 1'b1 : 1'b0;
-      pwm_out[2] <= (cntr < 75 ) ? 1'b1 : 1'b0;
-      pwm_out[2] <= (cntr < 90 ) ? 1'b1 : 1'b0;
-    end else  begin
-      cntr <= 0;
-    end
+
+      if (cntr < 100) begin
+        pwm_out[0] <= (cntr < 25 ) ? 1'b1 : 1'b0;
+        pwm_out[1] <= (cntr < 50 ) ? 1'b1 : 1'b0;
+        pwm_out[2] <= (cntr < 75 ) ? 1'b1 : 1'b0;
+        pwm_out[3] <= (cntr < 90 ) ? 1'b1 : 1'b0;
+      end else  begin
+        cntr <= 0;
+      end
+   end
   end
 endmodule
--- work/w_3/pwm/pwm_tb.v
+++ work/w_3/pwm/pwm_tb.v
@@ -1,22 +1,33 @@
+`timescale 1ns/1ns
+
 module pwm_tb();
 
   reg        clk;
+  reg        rst_n;
   wire [3:0] pwm_out;
+
 
   pwm dut(
           .clk(clk),
+          .rst_n(rst_n),
           .pwm_out(pwm_out)
          );
 
 
   initial begin
-    $dumpfile("test_pwm.vcd");
-    $dumpvars(0,pwm_tb);
-
     clk = 0;
+    rst_n = 0;
+    #5 rst_n =1;
+
     forever #5 clk= ~clk;
+  end
+
+  initial begin
+    $dumpfile("waves.vcd");
+    $dumpvars(0,pwm_tb);
 
     #5000 $finish;
   end
+
 
 endmodule
